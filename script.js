3720a53389280fb560f2fd929aa5c3151be4bf34
pedrosoares7 <soarespedro7@hotmail.com>
2024-02-27 16:18:53 +0000

    dogLaugh e dogGotDuck

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-mouse = document.querySelector('.mouse')
-
+mouse = document.querySelector('.mouse');
+let dog= document.getElementById("dog")
 
 window.addEventListener('mousemove', function(move){
     mouse.style.left = move.pageX + "px";
     mouse.style.top = move.pageY + "px";
 
 })
+
+function dogLaugh(){
+    let dogLaugh= document.getElementById("dogLaughContainer");
+    dogLaugh.classList.add("animate");
+    setTimeout(() => {dogLaugh.classList.remove("animate")}, 3000);
+}
+
+function dogGotDuck(){
+    let dogGotDuck= document.getElementById("dogGotDuckContainer");
+    dogGotDuck.classList.add("animate1");
+    setTimeout(() => {dogGotDuck.classList.remove("animate1")}, 3000);
+}
 
 
